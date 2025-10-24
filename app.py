187e968674f1303c17227a06139bad8221ae93af
Miguel Angel <mikicuellar20@gmail.com>
2025-10-24 17:11:37 -0400

    Import classes

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask
-from src.controllers.tmdb_controller import tmdb_bp
+from src.controllers.tmdb_controller import TMDBController
+from src.controllers.game_controller import GameController
+
+from src.services.tmdb_service import TMDBService
 from flask_cors import CORS
 from dotenv import load_dotenv
 
@@ -19,8 +22,16 @@ def create_app():
     app.config['TMDB_API_KEY'] = os.getenv('TMDB_API_KEY')
     app.config['TMDB_BASE_URL'] = os.getenv('TMDB_BASE_URL')
 
+    # Iniciar servicios
+    tmdb_service = TMDBService(api_key=app.config['TMDB_API_KEY'], base_url=app.config['TMDB_BASE_URL'])
+
+    # Iniciar controllers
+    tmdb_controller = TMDBController(tmdb_service)
+    game_controller = GameController(tmdb_service)
+
     # Registrar blueprints (controllers manejan la API)
-    app.register_blueprint(tmdb_bp, url_prefix='/tmdb')
+    app.register_blueprint(tmdb_controller.blueprint, url_prefix='/tmdb')
+    app.register_blueprint(game_controller.blueprint, url_prefix='/game')
 
     return app
 
